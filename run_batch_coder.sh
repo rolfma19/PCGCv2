5f45ad9a4a29a6330d6c8c1b4fb2481cc6b4235e
Wanhao Ma <rolfma@163.com>
2025-08-25 09:13:19 +0800

    更新run_batch_coder.sh

--- run_batch_coder.sh
+++ run_batch_coder.sh
@@ -1,2 +1,5 @@
+# python batch_coder.py \
+#     --filedir=/home/whma/Python/BasicPCP/exps/FusionPCGC/MEShuffleCompressionLossyL1R00_epoch46_new/outputs_infer
+
 python batch_coder.py \
-    --filedir=/home/whma/Python/BasicPCP/exps/FusionPCGC/MEShuffleCompressionLossyL1R00_epoch46_new/outputs_infer
+    --filedir=/home/whma/Python/BasicPCP/data/testset_ours > log.txt
